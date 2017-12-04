0c55d3bef48477deb763be7cf16ca5b8603dbe66
Assley <ashwafaw@gmail.com>
2017-12-04 16:30:49 -0500

    Fixes

--- src/commands/member/Invite.js
+++ src/commands/member/Invite.js
@@ -1,21 +1,18 @@
-const {
-    Command
-} = require('patron.js');
+const { Command } = require('patron.js');
 
 class Invite extends Command {
+  constructor() {
+    super({
+      names: ['invite', 'botinvite', 'serverinvite', 'invitation'],
+      groupName: 'member',
+      description: 'Sends the bot invite link and server invite link.',
+      guildOnly: false
+    });
+  }
 
-    constructor() {
-        super({
-            names: ['invite', 'botinvite', 'serverinvite', 'invitation'],
-            groupName: 'member',
-            description: 'Sends the bot invite link and server invite link.',
-            guildOnly: false
-        });
-    }
-
-    run(msg) {
-        return msg.channel.send('**Bot invite:**\nhttps://discordapp.com/oauth2/authorize?client_id=377886472017149953&scope=bot&permissions=8\n**GitHub Repository:**\nhttps://github.com/LumiteDubbz/Pixel\n**Support Server invite link:**\nhttps://discord.me/pixelsupport');
-    }
+  run(msg, args, text) {
+    return text.send('**Bot invite:**\nhttps://discordapp.com/oauth2/authorize?client_id=377886472017149953&scope=bot&permissions=8\n**GitHub Repository:**\nhttps://github.com/LumiteDubbz/Pixel\n**Support Server invite link:**\nhttps://discord.me/pixelsupport', { footer: { icon: msg.client.user.displayAvatarURL, text: msg.client.user.username }});
+  }
 }
 
 module.exports = new Invite();
