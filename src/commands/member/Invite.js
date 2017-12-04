ebe313d349ebd9629fb63679ea25a96cd5acfeb5
LumiteDubbz <31250645+LumiteDubbz@users.noreply.github.com>
2017-12-04 19:42:09 +0000

    New invite command.
    Give me some credit. I made this on ANDROID and uploaded it to my VPS via a random WinSCP type add for Android.

---
+++ src/commands/member/Invite.js
@@ -0,0 +1,21 @@
+const {
+    Command
+} = require('patron.js');
+
+class Invite extends Command {
+
+    constructor() {
+        super({
+            names: ['invite', 'botinvite', 'serverinvite', 'invitation'],
+            groupName: 'member',
+            description: 'Sends the bot invite link and server invite link.',
+            guildOnly: false
+        });
+    }
+
+    run(msg) {
+        return msg.channel.send('**Bot invite:**\nhttps://discordapp.com/oauth2/authorize?client_id=377886472017149953&scope=bot&permissions=8\n**GitHub Repository:**\nhttps://github.com/LumiteDubbz/Pixel\n**Support Server invite link:**\nhttps://discord.me/pixelsupport');
+    }
+}
+
+module.exports = new Invite();
