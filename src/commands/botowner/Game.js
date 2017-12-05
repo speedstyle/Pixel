b9ada970abb5cfaadf2993a72dec9bc4f5e6f173
LumiteDubbz <31250645+LumiteDubbz@users.noreply.github.com>
2017-12-05 19:28:18 +0000

    New command to change the bot's game.
    Changes to default when the bot joins a server, leaves a server or restarts.

---
+++ src/commands/botowner/Game.js
@@ -0,0 +1,41 @@
+const { Command, Argument } = require('patron.js');
+const client = require('../../structures/Client.js');
+const credentials = require('../../../credentials.json')
+
+class Game extends Command {
+    constructor() {
+        super({
+            names: ['game', 'setgame', 'gameset'],
+            groupName: 'botowner',
+            description: 'Changes the bot\'s game.',
+            guildOnly: false,
+            args: [
+                new Argument({
+                    key: 'game',
+                    name: 'game',
+                    type: 'string',
+                    example: 'with dank memes.',
+                    remainder: true
+                })
+            ]
+        });
+    }
+    
+    run(msg, args, text) {
+        const options = {
+            timestamp: true,
+            author: {
+                name: 'Game Commmand',
+                icon: msg.author.displayAvatarURL
+            }
+        };
+
+        if(args.game.length > 128) {
+            return text.sendError('I cannot set my game to that because it is over 128 characters.')
+        }
+        client.user.setGame(args.game)
+        return text.reply(`successfully set my game to ${args.game}! To change it again, type \`${credentials.prefix}game\``)
+    }
+}
+
+module.exports = new Game();
