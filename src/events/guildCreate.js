2dfc37c965ef7903b541e7c522180d5bc75c953c
Will <31250645+LumiteDubbz@users.noreply.github.com>
2017-12-09 20:04:19 +0000

    new message the bot says when joining a server

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -1,5 +1,9 @@
 const client = require('../structures/Client.js');
+const text = require('../utility/Text');
 
 client.on('guildCreate', guild => {
   client.user.setGame('on ' + client.guilds.size + ' servers | ' + client.config.prefix + 'help', 'https://twitch.tv/lumitedubbz');
+  if(guild.defaultChannel !== undefined) {
+    text.send('Hey there, I\'m Pixel. You can view all of my available commands by typing `p!help`.\nShould you need support, you can join **the official support server:** https://discord.me/pixelsupport\n**Report any issues here:** https://github.com/lumitedubbz/pixel/issues')
+  }
 });
