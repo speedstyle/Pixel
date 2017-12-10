f326099d93ff069cb27f8c6fe645aa2defe1f373
Will <31250645+LumiteDubbz@users.noreply.github.com>
2017-12-10 22:06:23 +0000

    start of skills

--- src/database/models/User.js
+++ src/database/models/User.js
@@ -2,8 +2,19 @@ class User {
   constructor(userId, guildId) {
     this.userId = userId;
     this.guildId = guildId;
+    this.health = 0;
+    this.skillPoints = 0,
+    this.skill = 0,
     this.level = 1;
     this.xp = 0;
+    this.skills = {
+      magic: 1,
+      damage: 1,
+      healing: 1,
+      craftmanship: 1,
+      intelligence: 1,
+      haste: 1
+     };
   }
 }
 
